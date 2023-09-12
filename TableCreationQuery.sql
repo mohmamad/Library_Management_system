6bc8865efbd464bdce2e32e1af7a0d2718319007
Mohammad Moghrabi <mohamad.isa.moghrabi@gmail.com>
2023-09-12 11:29:51 +0300

    solved the teble names

--- TableCreationQuery.sql
+++ TableCreationQuery.sql
@@ -3,20 +3,20 @@ CREATE TABLE [Books] (
   [Title] varchar(100),
   [Author] varchar(50),
   [ISBN] int,
-  [Published Date] Date,
+  [Published_Date] Date,
   [Genre] varchar(25),
-  [Shelf Location] varchar(50),
-  [Current Status] varchar(10),
+  [Shelf_Location] varchar(50),
+  [Current_Status] varchar(10),
   PRIMARY KEY ([BookID])
 );
 
 CREATE TABLE [Borrowers] (
   [BowrrowerID] int,
-  [First Name] varchar(20),
-  [Last Name] varchar(20),
+  [First_Name] varchar(20),
+  [Last_Name] varchar(20),
   [Email] varchar(50),
-  [Date Of Birth] date,
-  [Membership Date] date,
+  [Date_Of_Birth] date,
+  [Membership_Date] date,
   PRIMARY KEY ([BowrrowerID])
 );
 
@@ -24,14 +24,14 @@ CREATE TABLE [Loans] (
   [LoanID] int,
   [BookID] int,
   [BorrowerID] int,
-  [Date Borrowed] date,
-  [Due Date] date,
-  [Date Returned] date,
+  [Date_Borrowed] date,
+  [Due_Date] date,
+  [Date_Returned] date,
   PRIMARY KEY ([LoanID]),
   CONSTRAINT [FK_Loans.BookID]
     FOREIGN KEY ([BookID])
       REFERENCES [Books]([BookID]),
-  CONSTRAINT [FK_Loans.Date Borrowed]
+  CONSTRAINT [FK_Loans.Date_Borrowed]
     FOREIGN KEY ([BorrowerID])
       REFERENCES [Borrowers]([BowrrowerID])
 );
